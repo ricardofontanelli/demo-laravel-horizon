c9321ab4e77228fad8e847b3d5ef89984304ea53
Ricardo Fontanelli <ricardo.fontanelli@hotmail.com>
2017-09-20 11:11:16 -0300

    CHANGE update dependencies and package discovery

--- projects/laravel55/config/app.php
+++ projects/laravel55/config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        RicardoFontanelli\LaravelTelegram\TelegramServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -226,7 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Telegram' => RicardoFontanelli\LaravelTelegram\TelegramFacade::class,
     ],
 
 ];
